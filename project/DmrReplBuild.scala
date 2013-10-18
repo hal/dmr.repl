4b629db63467954073b9e6f08fd41eaa7a919f4e
Harald Pehl <harald.pehl@gmail.com>
2013-10-18 09:16:25 +0200

    Update git dependency to DMR.scala

--- project/DmrReplBuild.scala
+++ project/DmrReplBuild.scala
@@ -2,5 +2,5 @@ import sbt._
 
 object DmrReplBuild extends Build {
   lazy val root = Project("root", file(".")) dependsOn (dmrScala)
-  lazy val dmrScala = RootProject(uri("git://github.com/hpehl/dmr.scala.git"))
+  lazy val dmrScala = RootProject(uri("git://github.com/hal/dmr.scala.git"))
 }
