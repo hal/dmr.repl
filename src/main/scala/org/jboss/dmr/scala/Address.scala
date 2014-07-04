9f907822038090015a582171030651785b30d7f4
Harald Pehl <harald.pehl@gmail.com>
2014-07-04 08:25:20 +0200

    Change Address.toString

--- src/main/scala/org/jboss/dmr/scala/Address.scala
+++ src/main/scala/org/jboss/dmr/scala/Address.scala
@@ -2,5 +2,8 @@ package org.jboss.dmr.scala
 
 /** Helper class for holding the address tuple for a model node */
 case class Address(tuple: List[(String, String)]) {
+
   def /(address: Address) = Address(tuple ++ address.tuple)
+
+  override def toString: String = "/" + (tuple map { case (key, value) => s"$key=$value"} mkString "/")
 }
