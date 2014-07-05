ca5f014f1849f7a6b77a6e6c5cafcdf8dfee5da7
Harald Pehl <harald.pehl@gmail.com>
2014-07-06 00:38:06 +0200

    Fix Address ordering

--- src/main/scala/org/jboss/dmr/scala/Address.scala
+++ src/main/scala/org/jboss/dmr/scala/Address.scala
@@ -1,11 +1,13 @@
 package org.jboss.dmr.scala
 
+import scala.math.Ordered
+
 /** Helper class for holding the address tuple for a model node */
-case class Address(tuple: List[(String, String)]) extends Ordering[Address] {
+case class Address(tuple: List[(String, String)]) extends Ordered[Address] {
 
   def /(address: Address) = Address(tuple ++ address.tuple)
 
-  override def compare(x: Address, y: Address): Int = x.toString compare y.toString
+  override def compare(that: Address): Int = this.toString compare that.toString
 
   override def toString: String = "/" + (tuple map { case (key, value) => s"$key=$value"} mkString "/")
 }
