11b4ac49b033d8e40fe10d5799b06d69efc084ab
Harald Pehl <harald.pehl@gmail.com>
2014-07-06 00:24:25 +0200

    Address implements Ordered

--- src/main/scala/org/jboss/dmr/scala/Address.scala
+++ src/main/scala/org/jboss/dmr/scala/Address.scala
@@ -1,9 +1,11 @@
 package org.jboss.dmr.scala
 
 /** Helper class for holding the address tuple for a model node */
-case class Address(tuple: List[(String, String)]) {
+case class Address(tuple: List[(String, String)]) extends Ordering[Address] {
 
   def /(address: Address) = Address(tuple ++ address.tuple)
+
+  override def compare(x: Address, y: Address): Int = x.toString compare y.toString
 
   override def toString: String = "/" + (tuple map { case (key, value) => s"$key=$value"} mkString "/")
 }
